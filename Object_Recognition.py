356c796250742f2c0af9445a281584903b0bcc04
virenfern <virenfern2006@gmail.com>
2026-03-11 16:53:00 +0530

    Update Object_Recognition.py

--- Object_Recognition.py
+++ Object_Recognition.py
@@ -1,4 +1,4 @@
-#PROJECT IS ABOUT CONTROLLING MOUSE VIA OPEN CV
+
 import cv2 as cv#capture video from camera
 import numpy as np  
 
@@ -37,4 +37,5 @@ cv.destroyAllWindows()
 #img : The image where you want to draw the shapes
 #color : Color of the shape. for BGR, pass it as a tuple, eg: (255,0,0) for blue. For grayscale, just pass the scalar value.
 #thickness : Thickness of the line or circle etc. If -1 is passed for closed figures like circles, it will fill the shape. default thickness = 1
+
 #lineType : Type of line, whether 8-connected, anti-aliased line etc. By default, it is 8-connected. cv.LINE_AA gives anti-aliased line which looks great for curves.
